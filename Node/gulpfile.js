737fa882dabd5c8fa3789a19c5642026f4639f22
kuninaa19 <minchan0527@naver.com>
2020-05-07 19:37:11 +0900

    gulp 기본파일 생성

---
+++ Node/gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp'); // Gulp 모듈 호출
+// Gulp.task() 를 사용해 기본(Default) 테스크를 정의
+
+gulp.task('default', async function () {
+    console.log('gulp default 일이 수행되었습니다!!!');
+});
+
