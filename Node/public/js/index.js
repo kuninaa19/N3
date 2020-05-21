ab2fb6e746fa5faf6e67807dd0b146baea577fc7
kuninaa19 <minchan0527@naver.com>
2020-05-21 23:45:36 +0900

    (added) index.js 검색버튼 관련 메서드 생성

---
+++ Node/public/js/index.js
@@ -0,0 +1,9 @@
+const search = document.getElementById("searchButton");
+
+
+const roomSearch = ()=>{
+    document.getElementById("search").submit();
+};
+
+
+search.addEventListener("click",roomSearch);
