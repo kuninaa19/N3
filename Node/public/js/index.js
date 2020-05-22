742efbdfec97190110b8c994746fe20cf1afcfb2
kuninaa19 <minchan0527@naver.com>
2020-05-22 13:12:10 +0900

    (updated) index.js 글자수 확인 메서드추가, 검색폼 전송 메서드수정

--- Node/public/js/index.js
+++ Node/public/js/index.js
@@ -1,8 +1,28 @@
+// checkTyping() 검색창 타이핑개수 확인
+// roomSearch() 검색창내부에 작성된 내용있을때 폼 전송
+
 const search = document.getElementById("searchButton");
 
+const checkTyping = ()=>{
+    let place = document.getElementById("place").value;
+    let count = place.length;
+
+    if(count!==0){
+        return true;
+    }
+    else{
+        document.getElementById("place").focus();
+        return false;
+    }
+};
 
 const roomSearch = ()=>{
-    document.getElementById("search").submit();
+    let key = checkTyping();
+
+    if(key){
+        document.getElementById("search").submit();
+    }
+
 };
 
 
