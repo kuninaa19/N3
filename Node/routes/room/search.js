3a09bc973151551bc43e4b4d2b830f9709444902
kuninaa19 <minchan0527@naver.com>
2020-05-30 15:31:15 +0900

    (added) search.js 로그인했을때 네비바에 유저아이디 전달 추가, 로그인 안했을때는 검색지역 이름값만 전달

--- Node/routes/room/search.js
+++ Node/routes/room/search.js
@@ -3,15 +3,25 @@ const router = express.Router();
 
 import mysql from '../../conf/dbInfo';
 
-router.get('/:places', (req, res) => {
-    res.render('room/search');
+const checkAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    //로그인 안되어있을때는 그냥 검색만
+    const searchValue= req.query.place; // 검색 지역이름
+    res.render('room/search',{title:searchValue});
+};
+
+router.get('/', checkAuth, (req, res) => {
+    const nickname = req.user.nickname; // 유저 아이디
+    const searchValue= req.query.place; // 검색 지역이름
+
+    res.render('room/search',{title:searchValue,'nickname':nickname});
 });
 
-router.get('/', (req, res) => {
-
-    let searchValue= req.query.place;
-
-    res.render('room/search',{title:searchValue});
+//추후 폐기[검색은 쿼리스트링이 남아야함]
+router.get('/:places', (req, res) => {
+    res.render('room/search');
 });
 
 export default router;
