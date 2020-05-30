ebd473142b807e6bbb740e4798076863446b3b5f
kuninaa19 <minchan0527@naver.com>
2020-05-30 16:03:00 +0900

    (added) detail.js 방 세부페이지 관련 로그인확인부분 추가

--- Node/routes/room/detail.js
+++ Node/routes/room/detail.js
@@ -3,24 +3,32 @@ const router = express.Router();
 
 import mysql from '../../conf/dbInfo';
 
+//방 결제페이지 인증확인
 const checkAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
     //모달창 열기로 변경해야됨
-    res.redirect('/');
+    const params = req.params.number;
+    const url = '/rooms/'+params;
+    res.redirect(url);
 };
 
-const checkNotAuth = (req, res, next) => {
+//방 세부페이지 로그인확인
+const checkIsAuthenticated = (req, res, next) => {
     //인증허가됨
     if (req.isAuthenticated()) {
-        return res.redirect('/dashboard');
+        next();
     }
-    next();
+    const searchValue= req.query.place; // 호텔 이름
+    res.render('room/detail',{'hotel_name':searchValue});
 };
-//방 세부 페이지
-router.get('/:number',  (req, res) => {
-    res.render('room/detail');
+//방 세부 페이지 (쿼리스트링 지역이름 + 호텔방 이름)
+router.get('/:number',checkIsAuthenticated,  (req, res) => {
+    const nickname = req.user.nickname; // 유저 아이디
+    const searchValue= req.query.place; // 호텔 이름
+
+    res.render('room/detail',{'hotel_name':searchValue,'nickname':nickname});
 });
 
 //방 확인 및 결제 페이지
