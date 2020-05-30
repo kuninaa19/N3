3b7b172ddc6c50752f26643bef1aac3fd49c32ca
kuninaa19 <minchan0527@naver.com>
2020-05-30 16:04:43 +0900

    (added) trip.js 로그인확인코드 추가

--- Node/routes/user/trip.js
+++ Node/routes/user/trip.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 import mysql from '../../conf/dbInfo';
 
-router.get('/', (req, res) => {
-    res.render('user/trip');
+
+const checkAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    // alert창으로 로그인? 혹은 로그인모달창열기
+};
+
+router.get('/', checkAuth, (req, res) => {
+    const nickname = req.user.nickname;
+    res.render('user/trip',{'nickname':nickname});
 });
 
 export default router;
