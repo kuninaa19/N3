f60a965b0528db7e90bb0b4e3324e906e8da0f69
kuninaa19 <minchan0527@naver.com>
2020-05-30 16:04:04 +0900

    (added) message.js 로그인 확인관련 코드 추가

--- Node/routes/user/message.js
+++ Node/routes/user/message.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 import mysql from '../../conf/dbInfo';
 
-router.get('/', (req, res) => {
-    res.render('user/message');
+const checkAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    //모달창 열기로 변경해야됨
+};
+
+// 유저 로그인했을때만 메시지페이지
+router.get('/', checkAuth, (req, res) => {
+    const nickname = req.user.nickname;
+    res.render('user/message',{'nickname':nickname});
 });
 
 export default router;
