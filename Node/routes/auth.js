c64de6e091a14854dfae521dd582fba43ee8b942
kuninaa19 <minchan0527@naver.com>
2020-05-30 15:58:55 +0900

    (added) auth.js 로그인,회원가입관련 인증확인처리추가

---
+++ Node/routes/auth.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import passport from 'passport';
+import initPassport from "../conf/passport";
+initPassport(passport);
+
+const router = express.Router();
+
+//로그인을해도 메인화면으로 가는건 동일하지만 로그인시에는 닉네임이 떠야함
+router.post('/login',
+    passport.authenticate(
+        'local-login',
+        {
+            successRedirect: '/',
+            failureRedirect: '/',
+            failureFlash: true
+        }
+    ));
+
+router.post('/register',
+    passport.authenticate(
+        'local-register',
+        {
+            successRedirect: '/',
+            failureRedirect: '/',
+            failureFlash: true
+        }
+    ));
+
+export default router;
