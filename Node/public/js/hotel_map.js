f93454a528bed3d2c91c406ad8aac765bcadb9e5
kuninaa19 <minchan0527@naver.com>
2020-06-01 23:38:39 +0900

    (added) hotel_map.js 호텔세부페이지 map관련 script파일 따로 생성

---
+++ Node/public/js/hotel_map.js
@@ -0,0 +1,7 @@
+function initMap() {
+    const hotel = {lat: 3.159210, lng: 101.705190};
+    const map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 15, center: hotel});
+
+    const marker = new google.maps.Marker({position: hotel, map: map});
+}
