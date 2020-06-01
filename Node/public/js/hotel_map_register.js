404b6e64c63517e8f2f247b6fa5f53ce2cf50ceb
kuninaa19 <minchan0527@naver.com>
2020-06-01 23:53:54 +0900

    (added) hotel_map_register.js 호텔 검색을 위한 geocoding api 파일생성

---
+++ Node/public/js/hotel_map_register.js
@@ -0,0 +1,28 @@
+//대문자인식안됨
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: {lat: 3.15, lng: 101.685922}
+    });
+    var geocoder = new google.maps.Geocoder();
+
+    document.getElementById('submit').addEventListener('click', function() {
+        geocodeAddress(geocoder, map);
+    });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
