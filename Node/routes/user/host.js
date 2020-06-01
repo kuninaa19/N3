77c2bbbd9ab6b9f10aa97cfe5c3515aaf808e60b
kuninaa19 <minchan0527@naver.com>
2020-06-01 15:56:27 +0900

    (added) host.js 숙소호스트 관련 라우터 추가

---
+++ Node/routes/user/host.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import connection from '../../conf/dbInfo';
+const router = express.Router();
+
+const checkAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    //모달창 열기로 변경해야됨
+};
+
+// router.get('/', checkAuth, (req, res) => {
+// router.get('/',  (req, res) => {
+//     const nickname = req.user.nickname;
+//     res.render('user/message',{'nickname':nickname});
+// });
+
+// 호스트 방 등록
+router.get('/room/register',  (req, res) => {
+
+    res.render('room/register');
+});
+
+
+export default router;
