d016ae64b3ec890f37bff691cd2bda29b7c5e68a
kuninaa19 <minchan0527@naver.com>
2020-06-02 16:47:53 +0900

    (added) hotel_map_register.js 검색에 따라 위도 대입 메서드 추가

--- Node/public/js/hotel_map_register.js
+++ Node/public/js/hotel_map_register.js
@@ -1,28 +1,48 @@
-//대문자인식안됨
+// initMap() 지도 위치 초기화
+// geocodeAddress() 검색한 값에 따라 위치변경
+// changeLatLng() 위도 경도 대입
 
+//지도 위치 초기화
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: {lat: 3.15, lng: 101.685922}
     });
-    var geocoder = new google.maps.Geocoder();
 
-    document.getElementById('submit').addEventListener('click', function() {
+    let geocoder = new google.maps.Geocoder();
+
+    document.getElementById('submit').addEventListener('click', function () {
         geocodeAddress(geocoder, map);
     });
 }
 
+//검색한 값에 따라 위치변경
 function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('address').value;
-    geocoder.geocode({'address': address}, function(results, status) {
+    let address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function (results, status) {
         if (status === 'OK') {
+            resultsMap.setZoom(15);
             resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
+            let marker = new google.maps.Marker({
                 map: resultsMap,
                 position: results[0].geometry.location
             });
+            let address_lat = results[0].geometry.location.lat();	//위도
+            let address_lng = results[0].geometry.location.lng();	//경도
+
+
+            changeLatLng(address_lat, address_lng);
+
+            alert('주소명 : ' + address + '\n\n위도 : ' + address_lat + '\n\n경도 : ' + address_lng);
+
         } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+            alert('알파벳 대문자를 사용하지않고 정확한 이름을 입력해주세요: ' + status);
         }
     });
+}
+
+//위도 경도값 대입
+function changeLatLng(lat, lng) {
+    document.getElementById('map_let').innerHTML = lat;
+    document.getElementById('map_lng').innerHTML = lng;
 }
