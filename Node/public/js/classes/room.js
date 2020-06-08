441d3e7644f1da7ea0679f2c14f6f4a980dd9a05
kuninaa19 <minchan0527@naver.com>
2020-06-08 17:25:39 +0900

    (fixed) room.js 추가로 작성됐던 정보들 삭제

--- Node/public/js/classes/room.js
+++ Node/public/js/classes/room.js
@@ -1,17 +1,13 @@
 //  room/register.js 에서 방 정보를 담는 클래스
 class room {
-    constructor(name,country,region,location,simple_info,host_name,intro_info,image,value) {
-        this.simpleInfo=simple_info;
-        this.hostName=host_name;
-        this.introInfo=intro_info;
+    constructor(name,country,region,location,simple_info,intro_info,value,image) {
         this._name = name;
         this._country = country;
         this._region = region;
         this._location = location;
-        this._simple_info = simple_info;
-        this._host_name = host_name;
-        this._intro_info = intro_info;
-        this._image = image;
+        this.simpleInfo=simple_info;
+        this.introInfo=intro_info;
         this._value = value;
+        this._image = image;
     }
 }
