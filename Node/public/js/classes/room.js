72b2cddacb5c1a5888157727f47c80f260d6747d
kuninaa19 <minchan0527@naver.com>
2020-06-08 17:31:45 +0900

    (fixed) room.js 생성자 내부 불필요한 _ 작성된거 삭제

--- Node/public/js/classes/room.js
+++ Node/public/js/classes/room.js
@@ -1,13 +1,13 @@
 //  room/register.js 에서 방 정보를 담는 클래스
 class room {
     constructor(name,country,region,location,simple_info,intro_info,value,image) {
-        this._name = name;
-        this._country = country;
-        this._region = region;
-        this._location = location;
+        this.name = name;
+        this.country = country;
+        this.region = region;
+        this.location = location;
         this.simpleInfo=simple_info;
         this.introInfo=intro_info;
-        this._value = value;
-        this._image = image;
+        this.value = value;
+        this.image = image;
     }
 }
