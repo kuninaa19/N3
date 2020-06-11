4efad7675a888646ecb94e7d6dbb88725caca69d
kuninaa19 <minchan0527@naver.com>
2020-06-12 01:14:09 +0900

    (added) datepicker.js diffDate() 날짜 차이 계산 메소드 추가

--- Node/public/js/datepicker.js
+++ Node/public/js/datepicker.js
@@ -1,3 +1,5 @@
+//diffDate() 입실날짜와 퇴실날짜 차이 계산
+
 $(function() {
     $('input[name="daterange"]').daterangepicker({
         autoApply: true,
@@ -8,7 +10,22 @@ $(function() {
             format: 'YYYY/MM/DD'
         }
     }, function(start, end, label) {
-        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-        console.log(document.getElementsByClassName("calendar_btn").valueOf());
+        // console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        // console.log(document.getElementsByClassName("calendar_btn")[0].valueOf());
+        let startDay = start.format('YYYY-MM-DD');
+        let endDay = end.format('YYYY-MM-DD');
+        diffDate(startDay,endDay);
     });
 });
+
+const diffDate = (start,end)=>{
+    const startDate = start.split('-');
+    const endDate = end.split('-');
+    // 자바스크립트는 1개월 빼고 계산해야함
+    const sDate = new Date(parseInt(startDate[0]), parseInt(startDate[1])-1, parseInt(startDate[2]));
+    const eDate = new Date(parseInt(endDate[0]), parseInt(endDate[1])-1, parseInt(endDate[2]));
+    const diff = eDate.getTime()-sDate.getTime();
+
+    const diffTime = Math.floor(diff/(1000*60*60*24));
+    console.log(diffTime);
+};
