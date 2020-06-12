5aebb920ad1515893ee636948f2e7ece185a1182
kuninaa19 <minchan0527@naver.com>
2020-06-13 00:13:41 +0900

    (added) detail.js 예약하기 버튼 클릭 함수 생성

--- Node/public/js/room/detail.js
+++ Node/public/js/room/detail.js
@@ -1,2 +1,9 @@
+const reservationBtn = document.getElementById("greenBtn");
 
 
+const makeReservation = () =>{
+       console.log(document.getElementsByClassName("calendar_btn")[0].value);
+
+};
+
+reservationBtn.addEventListener("click",makeReservation);
