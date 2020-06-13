7770b3bae855db34ee98f43f89b6fe259383a146
kuninaa19 <minchan0527@naver.com>
2020-06-13 21:49:03 +0900

    (added) detail.js 예약하기 버튼눌러서 폼전송

--- Node/public/js/room/detail.js
+++ Node/public/js/room/detail.js
@@ -1,9 +1,15 @@
 const reservationBtn = document.getElementById("greenBtn");
 
+// 예약일자 지정후 폼전송
+const makeReservation = () => {
+    const formCheckIn = document.getElementsByName("checkin")[0].value;
+    const formCheckOut = document.getElementsByName("checkout")[0].value;
 
-const makeReservation = () =>{
-       console.log(document.getElementsByClassName("calendar_btn")[0].value);
-
+    if (formCheckIn === formCheckOut) {
+           alert('체크인과 체크아웃 날짜를 다르게 해주세요');
+    } else {
+        document.getElementById('frm').submit();
+    }
 };
 
-reservationBtn.addEventListener("click",makeReservation);
+reservationBtn.addEventListener("click", makeReservation);
