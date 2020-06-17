5bd92519013c3d305e5ee739274d46f01004ca28
kuninaa19 <minchan0527@naver.com>
2020-06-18 00:28:04 +0900

    (deleted) naver.js 네이버페이는 가맹점 등록해야 테스트 API 지원됨 그래서 삭제

--- Node/public/js/room/reservation_payment.js
+++ Node/public/js/room/reservation_payment.js
@@ -1,14 +1,11 @@
 // kakaoPay() 결제 링크 전송(카카오페이)
+
 const paymentBtn = document.getElementById("paymentBtn");
 
-paymentBtn.addEventListener("click",  () =>{
-    if(kakaoBtn.value === "clicked") {
+paymentBtn.addEventListener("click", () => {
+    if (kakaoBtn.value === "clicked") {
         kakaoPay();
-    }
-    else if(elNaverPayBtn.value==="clicked"){
-        // naverPay();
-
-    }else{
+    } else {
         alert('결제수단을 선택해 주세요');
     }
 });
