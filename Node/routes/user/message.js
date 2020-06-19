c682134386a8436122cde5cea9266b6978b5e22c
kuninaa19 <minchan0527@naver.com>
2020-06-19 17:57:24 +0900

    (added) message.ejs message.js 메시지정보가져오기& 정보나열 코드 추가

--- Node/routes/user/message.js
+++ Node/routes/user/message.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connection from '../../conf/dbInfo';
+
 const router = express.Router();
 
 const checkAuth = (req, res, next) => {
@@ -12,7 +13,30 @@ const checkAuth = (req, res, next) => {
 // 유저 로그인했을때만 메시지페이지
 router.get('/', checkAuth, (req, res) => {
     const nickname = req.user.nickname;
-    res.render('user/message',{'nickname':nickname});
+
+    //메시지 최초 10개 전달
+    const sql = 'select  a.* ,b.aid, b.date, b.item_name from `message` AS `a` INNER JOIN `order` AS `b` WHERE a.user_name = ? OR a.host_name = ?  GROUP BY a.time ORDER BY a.time DESC LIMIT 10';
+    connection.query(sql, [nickname, nickname], (err, row) => {
+        if (err) throw  err;
+
+        // 숙박 날짜만 가져와서 재저장
+        row.forEach((val) => {
+            val.date = JSON.parse(val.date);
+
+            // // 접속자가 호스트라면
+            // if(val.host_name===nickname){
+            //
+            // }
+            // else{
+            //
+            // }
+        });
+        console.log(row);
+
+
+        res.render('user/message', {'nickname': nickname, 'message': row});
+    });
+
 });
 
 export default router;
