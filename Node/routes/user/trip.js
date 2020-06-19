4cb5e1b0abae2a2d6f8d8d691a7ad9270a8763c5
kuninaa19 <minchan0527@naver.com>
2020-06-19 17:05:33 +0900

    (added) trip.js 사용자 숙소예약정보 가져오는 쿼리 추가

--- Node/routes/user/trip.js
+++ Node/routes/user/trip.js
@@ -1,19 +1,33 @@
 import express from 'express';
 import connection from '../../conf/dbInfo';
+
 const router = express.Router();
-
-
 
 const checkAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
-    // alert창으로 로그인? 혹은 로그인모달창열기
+    res.send("<script>alert('로그인후 접속 가능합니다.'); history.go(-1);</script>");
 };
 
 router.get('/', checkAuth, (req, res) => {
     const nickname = req.user.nickname;
-    res.render('user/trip',{'nickname':nickname});
+
+    //숙소 예약 정보가져옴 날짜순으로 정렬해야할지 구매순으로 정렬해야할지 생각
+    const sql = 'select a.id, a.item_name, a.date, b.region, b.image from `order` AS `a` INNER JOIN `room` AS `b` GROUP BY a.id ORDER BY a.id DESC';
+    connection.query(sql, nickname, (err, row) => {
+        if (err) throw  err;
+
+        // 숙박 날짜만 가져와서 재저장
+        row.forEach(function (val) {
+            val.date = JSON.parse(val.date);
+        });
+        console.log(row);
+
+        const count = row.length;
+
+        res.render('user/trip', {'nickname': nickname, 'rooms': row, 'count': count});
+    });
 });
 
 export default router;
