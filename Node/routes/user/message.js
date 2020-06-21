9d8857beda3bc5dd0f2ec2adb9a4c287518a4067
kuninaa19 <minchan0527@naver.com>
2020-06-21 18:53:46 +0900

    (added) message.js 상대시간 표기 추가했음

--- Node/routes/user/message.js
+++ Node/routes/user/message.js
@@ -3,6 +3,37 @@ import connection from '../../conf/dbInfo';
 
 const router = express.Router();
 
+// 시간관련 모듈
+import moment from 'moment';
+
+require('moment-timezone');
+// import timezone from 'moment-timezone';
+moment.tz.setDefault("Asia/Seoul");
+
+// 시간 45분 -> 60분 일 22시간 -> 24시간
+moment.relativeTimeThreshold('m', 60);
+moment.relativeTimeThreshold('h', 24);
+moment.relativeTimeThreshold('M', 30);
+
+moment.updateLocale('ko', {
+    relativeTime: {
+        future: "%s 후",
+        past: "%s 전",
+        s: "%d 초",
+        ss: "%d 초",
+        m: "%d 분",
+        mm: "%d 분",
+        h: "%d 시간",
+        hh: "%d 시간",
+        d: "%d 일",
+        dd: "%d 일",
+        M: "%d 달",
+        MM: "%d 달",
+        y: "%d 년",
+        yy: "%d 년"
+    }
+});
+
 const checkAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
@@ -19,10 +50,19 @@ router.get('/', checkAuth, (req, res) => {
     connection.query(sql, [nickname, nickname], (err, row) => {
         if (err) throw  err;
 
+        console.log('1순위 시간', moment(row[0].time).subtract(9, 'hours'));
+        console.log('LLLL 변환', moment(row[0].time).subtract(9, 'hours').format('LLLL'));
+        console.log('현재시간', moment().format());
+
         // 숙박 날짜만 가져와서 재저장
         row.forEach((val) => {
             val.date = JSON.parse(val.date);
 
+            // mysql에 저장된 시간에서 9시간을 빼고 비교해야 정확한 결과값이 나온다.
+
+            val.time = moment(val.time).subtract(9, 'hours').fromNow();
+
+            // relativeTime.push(moment(val.time).subtract(9, 'hours').fromNow());
             // // 접속자가 호스트라면
             // if(val.host_name===nickname){
             //
@@ -31,7 +71,8 @@ router.get('/', checkAuth, (req, res) => {
             //
             // }
         });
-        console.log(row);
+
+        console.log('상대시간', moment('2020-06-19 19:57',"YYYY-MM-DD HH:mm").fromNow());
 
 
         res.render('user/message', {'nickname': nickname, 'message': row});
