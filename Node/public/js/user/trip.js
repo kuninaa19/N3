57aa2205ff08a4ad29b720547522eb4cdbe3765d
kuninaa19 <minchan0527@naver.com>
2020-06-24 16:20:46 +0900

    (added) trip.js trip/detail.ejs 관련 클릭시 페이지이동을 위한 메소드추가

--- Node/public/js/user/trip.js
+++ Node/public/js/user/trip.js
@@ -1,5 +1,13 @@
 // detailTrip() 클릭시 여행 상세페이지로 이동
 
-const detailTrip = params =>{
+const detailTrip = params => {
     location.href = `https://hotelbooking.kro.kr/trip/${params}`;
+};
+
+const moveToMessage = params => {
+    location.href = `https://hotelbooking.kro.kr/message/${params}`;
+};
+
+const moveToRoomInfo = params => {
+    location.href = `https://hotelbooking.kro.kr/rooms/${params}`;
 };
