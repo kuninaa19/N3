53d523c7c495c03908b8ee16a37fc36386fa9054
kuninaa19 <minchan0527@naver.com>
2020-07-03 14:29:22 +0900

    (added) socket_io.js 룸 조인과 메시지 저장 관련코드 추가

--- Node/socket_io.js
+++ Node/socket_io.js
@@ -1,21 +1,61 @@
 import socket_io from "socket.io";
+import connection from './conf/dbInfo';
+import moment from 'moment';
+import timezone from 'moment-timezone'; // require('moment-timezone');
 
-module.exports = function (app) {
-    //8080서버를 읽을때마다 리스트에 사람 채워줌
+moment.tz.setDefault("Asia/Seoul");
 
+module.exports = (app) => {
     const io = socket_io.listen(app);
-    let socketList = [];
+    const socketList = [];
+    // const userList = []; // 채팅방에 참여한 인원리스트
 
-    io.on('connection', function (socket) {
-        // 특정 소켓에서 connection 이벤트가 발생할 시(소켓이 연결이 됐을때) socketList에 push해줍니다. socketlist에 추가
+    io.on('connection', (socket) => {
         socketList.push(socket);
-        console.log('안녕');
 
-        //소켓 미 연결시 연결해제시
-        socket.on('disconnect', function () {
-            // 특정 소켓에서 disconnect 이벤트가 발생할 시(소켓 연결이 해제됐을때) socketList에서 해당 socket을 삭제해줍니다
-            //소켓저장된 위치를 찾아서 잘라줌 1개(splice 추가 삭제 코드다름.)
-            console.log('잘ㅈ가');
+        socket.on('joinRoom', (data) => {
+            socket.join(data.room, () => {
+                console.log('joinRoom', data.room);
+                // userList.push({
+                //     socket : socket.id,  // 생성된 socket.id
+                //     room : data.room  // 접속한 채팅방의 이름
+                // });
+                // console.log(userList);
+                // console.log(socket.rooms);
+
+                // io.to(data).emit('data', `${data.room}에 접속했어요`);
+            });
+            // console.log('방 현재인원',io.sockets.adapter.rooms[data.room].length);
+            // console.log('방 현재참여자',io.sockets.adapter.rooms[data.room]);
+        });
+
+        // 클라이언트가 채팅 내용 전송
+        socket.on('msg', (data) => {
+            console.log('data', data);
+            // console.log('room', socket.rooms[data.room_id]);
+            // const room_id = data.room_id;
+            //
+            const info = {
+                room_id: data.room_id,
+                sender: data.sender,
+                content: data.content,
+                time: moment().format('YYYY-MM-DD HH:mm:ss')
+            };
+
+            const sql = 'insert into `chat` set ?';
+            connection.query(sql, info, (err, row) => {
+                if (err) throw  err;
+
+
+                // socket.emit('aa', '채팅방여러분께 안녕');
+                socket.broadcast.to(info.room_id).emit('aa', '채팅방여러분께 안녕');
+            });
+        });
+
+        //소켓 연결해제
+        socket.on('disconnect', () => {
+            console.log('채팅방 나가기');
+            //소켓저장된 위치를 찾아서 잘라줌 1개
             socketList.splice(socketList.indexOf(socket), 1);
         });
     });
