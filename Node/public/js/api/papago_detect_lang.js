e49594336f2484421e4d8dcf8aa64cb4334db607
kuninaa19 <minchan0527@naver.com>
2020-07-05 13:23:57 +0900

    (updated) papago_detect_lang.js 파파고 언어감지 요청을 위한 데이터 처리와 검사이후 처리코드 추가

--- Node/public/js/api/papago_detect_lang.js
+++ Node/public/js/api/papago_detect_lang.js
@@ -1,16 +1,16 @@
 // 파파고 언어감지 API AJAX 요청
-const detectLng = () => {
-// const detectLng = (data) => {
-    console.log('detectLng');
+const detectLng = (id,msg) => {
     const detect = new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
 
         xhr.open("POST", "https://hotelbooking.kro.kr/papago/detectLangs", true);
         xhr.setRequestHeader("Content-Type", "application/json");
 
-        const chat = {content : 'data'};
-        // const chat = {content : data};
-        console.log(chat);
+        const chat = {
+          room_id:id,
+          message:msg
+        };
+
         xhr.onload = () => resolve(xhr.responseText);
         xhr.onerror = () => reject(xhr.statusText);
 
@@ -18,10 +18,9 @@ const detectLng = () => {
     });
     detect.then((response) => {
         const data = JSON.parse(response);
-        const langCode = data.langCode;
-
-        console.log('langCode',langCode);
-
+        if(data.key===true){
+            location.href='/trip';
+        }
     }).catch(error => {
         console.log(`에러발생: ${error}`);
     });
