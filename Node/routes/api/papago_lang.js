6a7b48def2ea5528e1dce7c0a676da61b8331ceb
kuninaa19 <minchan0527@naver.com>
2020-07-05 13:25:33 +0900

    (updated) papago_lang.js 언어감지요청 받고 검사결과 DB에서 저장하는 메소드 추가 + 언어감지요청메소드 부분수정

--- Node/routes/api/papago_lang.js
+++ Node/routes/api/papago_lang.js
@@ -1,11 +1,29 @@
 import express from 'express';
+import connection from "../../conf/dbInfo";
 import config from '../../conf/config';
 import request from 'request-promise-native';
 
 const router = express.Router();
 
-const get_info = async () => {
+// 언어감지된 값 저장
+const storeLng = (room_id, result) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'UPDATE `message` AS `a`, `chat` AS `b` SET a.lang = ?,b.lang = ? WHERE a.room_id=? AND b.room_id = ?';
+        connection.query(sql, [result, result, room_id, room_id], (err, row) => {
+            if (err) throw  err;
+
+            resolve(true);
+        });
+    }).catch(error => {
+        console.log(`storeMessage 에러 발생: ${error}`);
+    });
+};
+
+//언어감지 API
+const detectLng = async (data, options, req, res) => {
     try {
+        options.form = {'query': data.message};
+
         const result = await request(options, (error, response, body) => {
             if (!error && response.statusCode === 200) {
                 return body;
@@ -15,7 +33,10 @@ const get_info = async () => {
             }
         });
         const val = JSON.parse(result);
-        res.json(val);
+
+        await storeLng(data.room_id, val.langCode);
+
+        res.send({key: true});
         // res.send("<script>parent.after(val); </script>");
     } catch (e) {
         console.log(e)
@@ -25,21 +46,18 @@ const get_info = async () => {
 router.post('/detectLangs', (req, res) => {
     const api_url = 'https://openapi.naver.com/v1/papago/detectLangs';
 
-    console.log(req.body);
-    const query = req.body.content;
+    const data = req.body;
 
     const options = {
         url: api_url,
         method: 'POST',
-        form: {'query': query},
         headers: {
             'X-Naver-Client-Id': config.oauth.naver.client_id,
             'X-Naver-Client-Secret': config.oauth.naver.client_secret
         }
     };
 
-
-    get_info();
+    detectLng(data, options, req, res);
 });
 
 export default router;
