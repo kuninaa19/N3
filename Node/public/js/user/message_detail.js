1718685c4cc7c89de4f1c20de9e022358fef3a4a
kuninaa19 <minchan0527@naver.com>
2020-07-05 22:05:43 +0900

    (added) message_detail.js 페이지접근시 로드되는 채팅정보들에 번역버튼리스너와 함수 추가

--- Node/public/js/user/message_detail.js
+++ Node/public/js/user/message_detail.js
@@ -9,6 +9,7 @@ const getLastPath = url => {
 
 const room = getLastPath(window.location.pathname);
 const sendBtn = document.getElementById('sendBtn');
+const translateBtn = document.querySelectorAll ('.translate_btn');
 const chatView = document.querySelector('.content');
 const userName = document.getElementById('userName').innerText;
 
@@ -24,18 +25,41 @@ sendBtn.addEventListener("click", function () {
     if (message.value === '') {
         return;
     } else {
-        const msgInfo = {
-            room_id: room,
-            sender: userName,
-            content: message.value
-        };
+        const msg = new msgInfo(room, userName, message.value);
 
         // 메시지 서버로 전송
-        socket.emit('msg', msgInfo);
+        socket.emit('msg', msg);
 
         //메시지창입력정보 초기화
         message.value = '';
 
         chatView.scrollTop = chatView.scrollHeight;
     }
+});
+
+//페이지접속시 생성된 채팅번역을 위한 메서드
+const checkLng= (e) => {
+    //채팅인덱스 얻기
+    const msgForm = e.parentElement.parentElement.parentElement.parentElement.getAttribute('data-item-id');
+
+    // 메시지작성자(상대방) 닉네임
+    const senderName = e.parentElement.parentElement.getElementsByClassName('name')[0].innerText;
+
+    // 채팅내용
+    const msgDetail = e.parentElement.querySelector('.message_detail').innerText;
+
+    // 언어코드
+   const lngCode = e.parentElement.querySelector('.message_detail').getAttribute('langCode');
+    console.log(lngCode);
+
+    if(lngCode==='null'){
+        console.log('null');
+    }else{
+        console.log('aa');
+    }
+};
+
+// 초기 채팅내역 번역버튼리스너
+Array.prototype.forEach.call(translateBtn, (btn) => {
+    btn.addEventListener("click", () => {checkLng(btn)}, false);
 });
