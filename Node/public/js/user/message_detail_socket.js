d5c17ff8866659e168b72dc36274701fd2d32dee
kuninaa19 <minchan0527@naver.com>
2020-07-05 20:56:36 +0900

    (updated) message_detail_socket.js message_list.ejs 언어코드저장하는 속성추가, 버튼관련클래스,id 수정(detail_socket)

--- Node/public/js/user/message_detail_socket.js
+++ Node/public/js/user/message_detail_socket.js
@@ -80,12 +80,14 @@ const makeMessageBox = (data) => {
         // 보낸사람과 접속한 유저아이디 동일할때는 번역버튼x
         if (data.sender !== userName) {
             //언어 클래스 메시지에 추가
+            messageDetailNode.setAttribute('langCode',"null");
+
             const translateBtn = document.createElement('div');
-            translateBtn.className = "translateBtn";
+            translateBtn.className = "translate_btn";
 
             const imgButtonNode = document.createElement('input');
             imgButtonNode.type = "button";
-            imgButtonNode.className = "img_button";
+            imgButtonNode.id = "imgBtn";
 
             translateBtn.appendChild(imgButtonNode);
             messageDetailExternalNode.appendChild(translateBtn);
