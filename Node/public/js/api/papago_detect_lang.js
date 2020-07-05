f1eccfd7e6865259ef4004760bae235b921fbd81
kuninaa19 <minchan0527@naver.com>
2020-07-05 16:31:45 +0900

    (added) papago_detect_lang.js 메서드설명 주석 작성

--- Node/public/js/api/papago_detect_lang.js
+++ Node/public/js/api/papago_detect_lang.js
@@ -1,4 +1,6 @@
 // 파파고 언어감지 API AJAX 요청
+// detectLng(x,y) 결제승인이후 전송할 메시지에 대한 언어감지
+
 const detectLng = (id,msg) => {
     const detect = new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
