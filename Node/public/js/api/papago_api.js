f1498dd79127a32f77c94e667c0415959e141e91
kuninaa19 <minchan0527@naver.com>
2020-07-06 13:26:55 +0900

    (updated) papago_detect_lang.js=> papago_api.js로 통합

---
+++ Node/public/js/api/papago_api.js
@@ -0,0 +1,55 @@
+// 채팅창에서 언어감지와 번역기능을 위해 사용
+// detectLng() 결제승인이후 전송할 메시지에 대한 언어감지, 채팅번역버튼 언어감지
+// translateLng() 언어번역
+
+const detectLng = (data) => {
+    const detect = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "https://hotelbooking.kro.kr/papago/detectLangs", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        const chat = data;
+
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(xhr.statusText);
+
+        xhr.send(JSON.stringify(chat));
+    });
+    detect.then((response) => {
+        const data = JSON.parse(response);
+        if(data.key===true){
+            location.href='/trip';
+        }
+    }).catch(error => {
+        console.log(`에러발생: ${error}`);
+    });
+};
+
+
+const translateLng = () => {
+    const detect = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "https://hotelbooking.kro.kr/papago/translate", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        const chat = {
+            room_id:id,
+            message:e
+        };
+
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(xhr.statusText);
+
+        xhr.send(JSON.stringify(chat));
+    });
+    detect.then((response) => {
+        const data = JSON.parse(response);
+        if(data.key===true){
+            location.href='/trip';
+        }
+    }).catch(error => {
+        console.log(`에러발생: ${error}`);
+    });
+};
