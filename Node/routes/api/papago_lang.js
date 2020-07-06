ad04696f5138394f19893a003582611874449f69
kuninaa19 <minchan0527@naver.com>
2020-07-06 16:13:53 +0900

    (added) papago_lang.js 언어번역관련 코드 추가

--- Node/routes/api/papago_lang.js
+++ Node/routes/api/papago_lang.js
@@ -62,9 +62,9 @@ const detectLng = async (data, options, req, res) => {
 };
 // 파파고 언어감지 API
 router.post('/detectLangs', (req, res) => {
-    const api_url = 'https://openapi.naver.com/v1/papago/detectLangs';
-
     const data = req.body;
+
+    const api_url = 'https://openapi.naver.com/v1/papago/detectLangs';
 
     const options = {
         url: api_url,
@@ -78,41 +78,54 @@ router.post('/detectLangs', (req, res) => {
     detectLng(data, options, req, res);
 });
 
+//언어 번역
+const transLng = async (data, options, req, res) => {
+    try {
+        if (data.lang === 'ko') {
+            options.form.target = 'en';
+        } else {
+            options.form.target = 'ko';
+        }
+
+        const result = await request(options, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                return body;
+            } else {
+                res.status(response.statusCode).end();
+                console.log('error = ' + response.statusCode);
+            }
+        });
+        const parsedResult = JSON.parse(result);
+
+        const transMsg = {
+            msg : parsedResult.message.result.translatedText,
+            target : options.form.target
+        };
+
+        return res.json(transMsg);
+
+    } catch (e) {
+        console.log(e)
+    }
+};
+
 // 파파고 언어번역 API
 router.post('/translate', (req, res) => {
-    console.log(req.body);
-    const query = req.body;
+    const data = req.body;
 
     const api_url = 'https://openapi.naver.com/v1/papago/n2mt';
 
     const options = {
         url: api_url,
         method: 'POST',
-        form: {'source': 'en', 'target': 'ko', 'text': query.number1},
-        headers: {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret}
-    };
-
-    const get_info = async () => {
-        try {
-            const result = await request(options, (error, response, body) => {
-                if (!error && response.statusCode === 200) {
-                    return body;
-                } else {
-                    res.status(response.statusCode).end();
-                    console.log('error = ' + response.statusCode);
-                }
-            });
-            const val = JSON.parse(result);
-            console.log(val.message.result.translatedText);
-
-            // return res.json(val.message.result.translatedText);
-            return res.json(val);
-        } catch (e) {
-            console.log(e)
+        form: {'source': data.lang, 'target': '', 'text': data.message},
+        headers: {
+            'X-Naver-Client-Id': config.oauth.naver.client_id,
+            'X-Naver-Client-Secret': config.oauth.naver.client_secret
         }
     };
 
-    get_info();
+    transLng(data, options, req, res);
 });
 
 export default router;
