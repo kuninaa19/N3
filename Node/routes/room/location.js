7277c47db632e2f26de52aa9d50aa4fa2ac964ab
kuninaa19 <minchan0527@naver.com>
2020-07-07 21:57:55 +0900

    (updated) location.js 코드재활용하려고 쿼리변경

--- Node/routes/room/location.js
+++ Node/routes/room/location.js
@@ -5,13 +5,11 @@ const router = express.Router();
 
 // 위치정보얻기
 const getLanLng = (data, res) => {
-    const sql = 'select b.location from `order` AS `a` INNER JOIN `room` AS `b` ON a.item_name=b.name WHERE a.aid=? AND a.item_name=?';
-    connection.query(sql, [data.code, data.hotelName], (err, row) => {
+    const sql = 'select location from room  WHERE name=?';
+    connection.query(sql, data.hotelName, (err, row) => {
         if (err) throw  err;
 
-        console.log(row);
         const location = JSON.parse(row[0].location);
-        console.log(location);
         res.json(location);
     });
 };
@@ -19,7 +17,6 @@ const getLanLng = (data, res) => {
 //숙소 위치 전달
 router.post('/', (req, res) => {
     const data = req.body;
-    console.log(data);
 
     getLanLng(data, res)
 });
