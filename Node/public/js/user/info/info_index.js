827a249038349bbc8485cc18dfbc877c133a86cf
kuninaa19 <minchan0527@naver.com>
2020-07-14 22:12:44 +0900

    (added) info_index.js 프로필 메인페이지 JS 버튼이벤트리스너 추가

---
+++ Node/public/js/user/info/info_index.js
@@ -0,0 +1,24 @@
+
+const userInfoBtn = document.querySelectorAll('.user_info_category'); // 좌측 유저 개인정보 카테고리
+
+const writtenReviewBtn = document.getElementById('borderLine'); // 작성했떤 리뷰페이지로 이동
+
+const makeReviewBtn = document.getElementById('reviewBtn'); // 리뷰작성버튼
+
+
+
+userInfoBtn[0].addEventListener("click",function (){
+
+});
+
+userInfoBtn[1].addEventListener("click",function (){
+
+});
+
+writtenReviewBtn.addEventListener("click",function (){
+    location.href = '/info/review';
+});
+
+makeReviewBtn.addEventListener("click",function (){
+
+});
