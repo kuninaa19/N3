b1cb6318a549c63f2f7b1f07a51e6629421f31be
kuninaa19 <minchan0527@naver.com>
2020-07-16 19:52:01 +0900

    (added) info_index.js 리뷰작성버튼 클릭시 리뷰작성창 활성화

--- Node/public/js/user/info/info_index.js
+++ Node/public/js/user/info/info_index.js
@@ -1,24 +1,28 @@
-
 const userInfoBtn = document.querySelectorAll('.user_info_category'); // 좌측 유저 개인정보 카테고리
-
 const writtenReviewBtn = document.getElementById('borderLine'); // 작성했떤 리뷰페이지로 이동
+const makeReviewBtn = document.querySelectorAll('.review_btn'); // 리뷰작성버튼
 
-const makeReviewBtn = document.getElementById('reviewBtn'); // 리뷰작성버튼
-
-
-
-userInfoBtn[0].addEventListener("click",function (){
-
+userInfoBtn[0].addEventListener("click", function () {
 });
 
-userInfoBtn[1].addEventListener("click",function (){
-
+userInfoBtn[1].addEventListener("click", function () {
 });
 
-writtenReviewBtn.addEventListener("click",function (){
+writtenReviewBtn.addEventListener("click", function () {
     location.href = '/info/review';
 });
 
-makeReviewBtn.addEventListener("click",function (){
+const popUp = (e) => {
+    const itemId = e.getAttribute('data-item-id');
 
+    var url = "/info/review/writing";
+    var name = "hotelbooking";
+    var option = "width = 700, height = 600, top = 35%, left = 100%, location = no";
+    window.open(url, name, option);
+};
+
+Array.prototype.forEach.call(makeReviewBtn, (btn) => {
+    btn.addEventListener("click", () => {
+        popUp(btn)
+    }, false);
 });
