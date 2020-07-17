3f297ed9ed076cc09e508f28c01cdbc79d89afe2
kuninaa19 <minchan0527@naver.com>
2020-07-17 19:05:36 +0900

    (added) info_review_writing.js 리뷰작성완료버튼 AJAX요청 메서드 추가

--- Node/public/js/user/info/info_review_writing.js
+++ Node/public/js/user/info/info_review_writing.js
@@ -1,19 +1,86 @@
-const rateStarBtn = document.querySelectorAll('.rate_star'); // 평점
+// starOnOff() 클릭이벤트리스너 - 숙소평점주기
+// sendReview 폼전송 - 리뷰작성완료
+// closeReview 창닫기
 
+const rateStarBtn = document.querySelectorAll('.rate_star'); // 평점
+const confirmBtn = document.querySelector('.confirm'); // 리뷰폼 전송
+const closeBtn = document.querySelector('.close'); // 리뷰작성 취소
+
+// 클릭이벤트리스너 - 숙소평점주기
 const starOnOff = (e) => {
     const score = parseInt(e.getAttribute('rate')) - 1;
 
+    // 평점처음선택 or 기존에 클릭한 평점보다 평점 상승
     if (e.className === "rate_star") {
-        console.log('starOn');
         for (let i = score; i > -1; i--) {
             rateStarBtn[i].className = "active_rate_star";
         }
     } else {
-        console.log('starOff');
+        // 기존에 클릭한 평점보다 평점 하락
         for (let i = score + 1; i < 5; i++) {
             rateStarBtn[i].className = "rate_star";
         }
     }
+};
+
+//유저가 평점을 남긴여부 확인후 평점 반환
+const checkRate = async () => {
+    const rate = document.getElementsByClassName('active_rate_star').length;
+
+    if (rate === 0) {
+        alert('숙소에대한 평점을 남겨주세요');
+        return false;
+    } else {
+        return rate;
+    }
+};
+
+//리뷰작성폼 전송
+const sendReview = (reviewRate, orderId) => {
+    return new Promise((resolve, reject) => {
+        const baseUrl = "https://hotelbooking.kro.kr";
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', baseUrl + '/info/review/storage', true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        const data = {
+            order_id: orderId,
+            score: reviewRate,
+            room_name: document.querySelector('.interval').innerText,
+            content: document.querySelector('.message').value
+        };
+
+        xhr.onload = () => resolve(xhr.responseText);
+        xhr.onerror = () => reject(xhr.statusText);
+
+        xhr.send(JSON.stringify(data));
+    }).then((response) => {
+        const result = JSON.parse(response);
+        if (result.key) {
+            opener.location.replace('/info/review');
+            window.close();
+
+        } else {
+            alert('다시시도 해주세요');
+        }
+    });
+};
+
+// 리뷰작성 폼 전송준비 메소드
+const readyToSend = async (e) => {
+    const rate = await checkRate();
+
+    if (rate) {
+        const orderId = e.target.getAttribute('data-order-id');
+
+        await sendReview(rate, orderId);
+    }
+};
+
+// 리뷰작성창 닫기
+const closeReview = () => {
+    window.close();
 };
 
 Array.prototype.forEach.call(rateStarBtn, (btn) => {
@@ -21,3 +88,8 @@ Array.prototype.forEach.call(rateStarBtn, (btn) => {
         starOnOff(btn)
     }, false);
 });
+
+confirmBtn.addEventListener("click", (e) => {
+    readyToSend(e)
+});
+closeBtn.addEventListener("click", closeReview);
