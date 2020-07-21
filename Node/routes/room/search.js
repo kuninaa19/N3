f0ae93645398ac9c056108da92cdb3e9f804f5a9
kuninaa19 <minchan0527@naver.com>
2020-07-21 19:34:37 +0900

    (updated) search.js / 1. 검색된 숙소리스트 평점,후기 개수 가져오도록 쿼리수정 2. checkAuth() 삭제

--- Node/routes/room/search.js
+++ Node/routes/room/search.js
@@ -1,48 +1,44 @@
 import express from 'express';
 import connection from '../../conf/dbInfo';
+
 const router = express.Router();
 
+// 검색한 지역 숙소정보 리스트 가져오기
+const getRoomList = (searchValue) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT a.id,a.name,a.region,a.value,a.image,(SELECT COUNT(*) FROM `review` WHERE a.name = room_name) as count,(SELECT SUM(score) FROM `review` WHERE a.name = room_name) as score FROM `room` as a  WHERE a.region = ?';
+        // const sql = 'select id,name,region,value,image from `room` where region = ?';
+        connection.query(sql, searchValue, (err, row) => {
+            if (err) throw  err;
 
-const checkAuth = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    //로그인 안되어있을때는 그냥 검색만
-    const searchValue= req.query.place; // 검색 지역이름
-
-    const sql = 'select `id`,`name`,`region`,`value`,`image` from `room` where region = ?';
-    connection.query(sql,searchValue,(err, row) => {
-        if (err) throw  err;
-
-        // 하루 숙박비 값만 가져와서 재저장
-        row.forEach(function(val){
-            val.value =JSON.parse(val.value).perDay;
+            // 하루 숙박비 값만 가져와서 재저장, 평점 재저장
+            row.forEach(function (val) {
+                val.value = JSON.parse(val.value).perDay;
+                if (val.count === 0) {
+                    val.score = 0;
+                } else {
+                    val.score = (val.score / val.count);
+                }
+            });
+            resolve(row);
         });
-
-        res.render('room/search',{title:searchValue,'rooms':row});
+    }).catch(error => {
+        console.log(`SQL 에러 발생: ${error}`);
+        return false;
     });
 };
 
-router.get('/', checkAuth, (req, res) => {
-    const nickname = req.user.nickname; // 유저 아이디
-    const searchValue= req.query.place; // 검색 지역이름
+router.get('/', async (req, res) => {
+    const searchValue = req.query.place; // 검색 지역이름
 
-    const sql = 'select * from `room` where region = ?';
-    connection.query(sql,searchValue,(err, row) => {
-        if (err) throw  err;
+    const roomList = await getRoomList(searchValue);
 
-        // 하루 숙박비 값만 가져와서 재저장
-        row.forEach(function(val){
-            val.value =JSON.parse(val.value).perDay;
-        });
-
-        res.render('room/search',{title:searchValue,'nickname':nickname,'rooms':row});
-    });
-});
-
-//추후 폐기[검색은 쿼리스트링이 남아야함]
-router.get('/:places', (req, res) => {
-    res.render('room/search');
+    if (req.isAuthenticated()) {
+        const nickname = req.user.nickname; // 유저 아이디
+        res.render('room/search', {title: searchValue, 'nickname': nickname, 'rooms': roomList});
+    } else {
+        res.render('room/search', {title: searchValue, 'rooms': roomList});
+    }
 });
 
 export default router;
