cfaa51446cd2bc69c71f3746da77b83929bbfaca
kuninaa19 <minchan0527@naver.com>
2020-07-21 17:11:10 +0900

    (added) search_modal.js / 검색창모달 클릭 이벤트리스너 추가

--- Node/public/js/modal/search_modal.js
+++ Node/public/js/modal/search_modal.js
@@ -1,11 +1,13 @@
 // openSearchModal() 검색창 모달 열기
 //closeSearchModal()검색창 모달닫기
 // clickNav() 네비바 클릭시 검색창 모달닫기
+// inputPlace() 모달에서 숙박하려는 지역 클릭시 검색창에 띄워줌
 
 const searchModalBtn = document.getElementById("place"); // 검색창
 const searchModal = document.querySelector(".search_modal");
 const searchModalOverlay = searchModal.querySelector(".search_modal_overlay");
 const topNav = document.querySelector(".top_nav");
+const countryBox = document.querySelectorAll('.country_box');
 
 //검색창 모달 열기
 const openSearchModal = () => {
@@ -13,15 +15,28 @@ const openSearchModal = () => {
 };
 
 //검색창 모달닫기
-const closeSearchModal = ()=>{
+const closeSearchModal = () => {
     searchModal.classList.add("hidden");
 };
 
 // 네비바 클릭시 검색창 모달닫기
-const clickNav = ()=>{
+const clickNav = () => {
     searchModal.classList.add("hidden"); // 검색모달창 열려있다면 닫음
 };
 
+// 모달에서 숙박하려는 지역 클릭시 검색창에 보여줌
+const inputPlace = (e) => {
+    searchModal.classList.add("hidden");
+    const place = document.getElementById('place');
+    place.value= e.getAttribute('country-id');
+};
+
 searchModalBtn.addEventListener("click", openSearchModal);
-searchModalOverlay.addEventListener("click",closeSearchModal);
-topNav.addEventListener("click",clickNav);
+searchModalOverlay.addEventListener("click", closeSearchModal);
+topNav.addEventListener("click", clickNav);
+
+Array.prototype.forEach.call(countryBox, (btn) => {
+    btn.addEventListener("click", () => {
+        inputPlace(btn)
+    }, false);
+});
