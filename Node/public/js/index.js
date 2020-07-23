f5f7a1d56b38b0d0079255bce6d770f797334cf6
kuninaa19 <minchan0527@naver.com>
2020-07-23 15:01:22 +0900

    (added) index.js / 1. 추천여행지 카드 리스너 추가 2. 숙소리스트 페이지로 이동하는 메서드 추가

--- Node/public/js/index.js
+++ Node/public/js/index.js
@@ -1,4 +1,19 @@
 // 숙소 세부정보 링크 이동
-const roomDetail = value =>{
+const roomDetail = value => {
     location.href = `https://hotelbooking.kro.kr/rooms/${value}`;
 };
+
+// 여행지 숙소 리스트 페이지로 이동
+const searchPlace = (e) => {
+    const location = e.getElementsByTagName('b')[0].innerText;
+    window.location.href = `https://hotelbooking.kro.kr/search?place=${location}`;
+};
+
+const place = document.querySelectorAll('#recommendation');
+
+// 추천하는 여행지 카드 리스너
+Array.prototype.forEach.call(place, (btn) => {
+    btn.addEventListener("click", () => {
+        searchPlace(btn);
+    }, false);
+});
