c1aac1097cca08616a9aacb656fd58ed19988015
kuninaa19 <minchan0527@naver.com>
2020-07-23 18:15:28 +0900

    (added) withdrawal_module.js / 카카오 회원탈퇴 로직 추가

--- Node/routes/withdrawal_module.js
+++ Node/routes/withdrawal_module.js
@@ -1,5 +1,6 @@
 import request from "request-promise-native";
 import connection from "../conf/dbInfo";
+import config from "../conf/config";
 
 const deleteUser = (data, req, res) => {
     return new Promise((resolve, reject) => {
@@ -19,21 +20,55 @@ const deleteUser = (data, req, res) => {
         return false;
     });
 };
+const setOptions = (token) => {
+    const options = {
+        url: 'https://kapi.kakao.com/v1/user/unlink',
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+    };
+    return options;
+};
+
+const unlink = async (options) => {
+    try {
+        await request(options, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                return body;
+            }
+        });
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+};
 
 //  회원탈퇴
 const userWithdrawal = async (req, res) => {
     const data = {
         token: req.cookies.accessToken,
-        platform: req.user.way
+        platform: req.user.way,
+        nickname: req.user.nickname
     };
-
     if (data.platform === 'general') {
-        const data = req.user.nickname;
-        const result = await deleteUser(data, req, res); // 일반로그인 회원젇보 삭제
+        const result = await deleteUser(data.nickname, req, res); // 일반로그인 회원젇보 삭제
 
         return result;
     } else {
-        await unlinkKakao(data, req, res); // 정보가져옴 유효하지않으면 재발급
+        const options = await setOptions(data.token); // 옵션설정
+        const key = await unlink(options); // 카카오 링크해제
+
+        console.log(key);
+        if (key) {
+            const result = await deleteUser(data.nickname, req, res); // 일반로그인 회원젇보 삭제
+
+            return result;
+        } else {
+            return key;
+        }
     }
 };
 
