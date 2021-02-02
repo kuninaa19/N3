217cb30b52a313b77027d8235a821596ca474c8a
kuninaa19 <minchan0527@naver.com>
2021-02-02 13:14:12 +0900

    app.js 간격 수정

--- node/src/app.js
+++ node/src/app.js
@@ -1,18 +1,19 @@
-import express from "express";
-import socket from "./socket_io";
-import loaders from "./loaders";
-import config from "./conf/config";
-import logger from "./loaders/logger";
+import express from 'express';
+import socket from './socket_io';
+import loaders from './loaders';
+import config from './conf/config';
+import logger from './loaders/logger';
 
 async function startServer() {
     const app = express();
 
     await loaders(app);
 
-    const server = app.listen(config.port, () => {
-        logger.info('Server listening on port : %o', config.port);
-    })
-        .on("error", (err) => {
+    const server = app
+        .listen(config.port, () => {
+            logger.info('Server listening on port : %o', config.port);
+        })
+        .on('error', (err) => {
             logger.error(err);
             process.exit(1);
         });
